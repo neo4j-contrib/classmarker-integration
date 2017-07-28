fd3e857911901a92514e90d295448131bee0af8b
Mark Needham <mark.needham@neotechnology.com>
2017-07-28 16:17:54 +0100

    convert to pdf using code from https://github.com/qoda/python-wkhtmltopdf

--- handler.py
+++ handler.py
@@ -3,18 +3,12 @@ import flask
 import base64
 
 from flask import render_template, url_for
+from lib.wkhtmltopdf import wkhtmltopdf
 
 app = flask.Flask('my app')
 
 def generate_certificate(event, context):
-    print("generating certificate...", event)
-
-    name = event["name"]
-    score_percentage = event["score_percentage"]
-    score_absolute = event["score_absolute"]
-    score_maximum = event["score_maximum"]
-    date = event["date"]
-    user_id = event["user_id"]
+    print("Generating certificate for {event}".format(event = event))
 
     with app.app_context():
         with open("static/neo4j.png", "rb") as neo4j_image:
@@ -22,16 +16,20 @@ def generate_certificate(event, context):
 
         rendered = render_template('certificate.html', \
             base_64_image = base_64_image.decode("utf-8"), \
-            name = name, \
+            name = event["name"], \
             test_name = "Neo4j Certification", \
-            score_percentage = score_percentage, \
-            score_absolute = score_absolute, \
-            score_maximum = score_maximum, \
-            date = date)        
+            score_percentage = event["score_percentage"], \
+            score_absolute = event["score_absolute"], \
+            score_maximum = event["score_maximum"], \
+            date = event["date"])
 
-        local_file_name = "/tmp/{file_name}.html".format(file_name=user_id)
-        with open(local_file_name, "wb") as file:
+        local_html_file_name = "/tmp/{file_name}.html".format(file_name=event["user_id"])
+        with open(local_html_file_name, "wb") as file:
             file.write(rendered.encode('utf-8'))
+
+        local_pdf_file_name = "/tmp/{file_name}.pdf".format(file_name=event["user_id"])
+        wkhtmltopdf(local_html_file_name, local_pdf_file_name)
+
 
         # s3_connection = boto.connect_s3()
         # bucket = s3_connection.get_bucket(short_name)
---
+++ lib/wkhtmltopdf.py
@@ -0,0 +1,64 @@
+import os
+from subprocess import Popen
+from subprocess import PIPE
+
+class WKhtmlToPdf(object):
+    """
+    Convert an html page via its URL into a pdf.
+    """
+    def __init__(self, *args, **kwargs):
+        self.url = None
+        self.output_file = None
+
+        # get the url and output_file options
+        try:
+            self.url, self.output_file = args[0], args[1]
+        except IndexError:
+            pass
+
+        if not self.url or not self.output_file:
+            raise Exception("Missing url and output file arguments")
+
+        # save the file to /tmp if a full path is not specified
+        output_path = os.path.split(self.output_file)[0]
+        if not output_path:
+            self.output_file = os.path.join('/tmp', self.output_file)
+
+
+
+        self.params = []
+        self.screen_resolution = [1024, 768]
+        self.color_depth = 24
+
+    def render(self):
+        """
+        Render the URL into a pdf and setup the evironment if required.
+        """
+        
+        # execute the command
+        command = 'wkhtmltopdf %s "%s" "%s" >> /tmp/wkhtp.log' % (
+                        " ".join([cmd for cmd in self.params]),
+                        self.url,
+                        self.output_file
+                  )
+        try:
+            p = Popen(command, shell=True,
+                        stdout=PIPE, stderr=PIPE, close_fds=True)
+            stdout, stderr = p.communicate()
+            retcode = p.returncode
+
+            if retcode == 0:
+                # call was successful
+                return
+            elif retcode < 0:
+                raise Exception("terminated by signal: ", -retcode)
+            else:
+                raise Exception(stderr)
+
+        except OSError as exc:
+            raise exc
+
+
+def wkhtmltopdf(*args, **kwargs):
+    wkhp = WKhtmlToPdf(*args, **kwargs)
+    wkhp.render()
