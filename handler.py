7ac65d70ee9d1658afc6dc0d2b5d9714ff282f00
Mark Needham <mark.needham@neotechnology.com>
2017-07-28 16:46:01 +0100

    generate PDf

--- handler.py
+++ handler.py
@@ -4,11 +4,13 @@ import base64
 
 from flask import render_template, url_for
 from lib.wkhtmltopdf import wkhtmltopdf
+from boto.s3.connection import ProtocolIndependentOrdinaryCallingFormat
 
 app = flask.Flask('my app')
 
 def generate_certificate(event, context):
     print("Generating certificate for {event}".format(event = event))
+    user_id = event["user_id"]
 
     with app.app_context():
         with open("static/neo4j.png", "rb") as neo4j_image:
@@ -23,15 +25,17 @@ def generate_certificate(event, context):
             score_maximum = event["score_maximum"], \
             date = event["date"])
 
-        local_html_file_name = "/tmp/{file_name}.html".format(file_name=event["user_id"])
+        local_html_file_name = "/tmp/{file_name}.html".format(file_name = user_id)
         with open(local_html_file_name, "wb") as file:
             file.write(rendered.encode('utf-8'))
 
-        local_pdf_file_name = "/tmp/{file_name}.pdf".format(file_name=event["user_id"])
+        local_pdf_file_name = "/tmp/{file_name}.pdf".format(file_name = user_id)
         wkhtmltopdf(local_html_file_name, local_pdf_file_name)
 
+        bucket_name = "training-certificates.neo4j.com"
 
-        # s3_connection = boto.connect_s3()
-        # bucket = s3_connection.get_bucket(short_name)
-        # key = boto.s3.key.Key(bucket, "{summary}.html".format(summary=short_name))
-        # key.set_contents_from_filename(local_file_name)
+        s3_connection = boto.connect_s3(calling_format=ProtocolIndependentOrdinaryCallingFormat())
+        bucket = s3_connection.get_bucket(bucket_name)
+        key = boto.s3.key.Key(bucket, "{user_id}.pdf".format(user_id=event["user_id"]))
+        key.set_contents_from_filename(local_pdf_file_name)
+        print("https://s3.amazonaws.com/{bucket_name}/{user_id}.pdf".format(bucket_name = bucket_name, user_id = user_id))
